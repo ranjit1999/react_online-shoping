7e36a75cd317783bc3fe6a85f07f817e8a151ae7
ranjit1999 <201751041@iiitvadodara.ac.in>
2020-06-03 22:26:51 +0530

    online shoping project

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const Footer = props => {
+  return (
+    <footer>
+      <p>
+        &copy; 2020 <strong>Veggy</strong> - Made by <strong>Ranjit Kumar</strong>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
